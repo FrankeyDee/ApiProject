2eba3f715e166802c0b24413689434baebb9f4ac
frankey darban <frankeydarban@gmail.com>
2019-07-27 14:10:43 -0700

    jAVA

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+//Create Array for Restaurants
+    var restaurants = [
+        'restaurant 1',
+        'restaurant 2',
+        'restaurant 3',
+    ];
+//Create Array for Tweets
+    var tweets = [
+        'tweet 1',
+        'tweet 2',
+        'tweet 3',
+    ];
+//Capture value for Search: Where
+
+    //Link to OpenTable (Ajax Call) 
+
+    //Result of Search from OpenTable (limit 10)
+
+//Capture value for Search: What
+
+    //Link to Twitter (AJAX)
+
+    //Create 'search phrase' from both WHERE and WHAT
+
+    //Results of 'search phrase' from Twitter
+
+//Display result of 'Where'
+
+//Display result of 'What'
+
+});
