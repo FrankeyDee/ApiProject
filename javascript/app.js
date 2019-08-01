a1c16587b7ff912474aefbf859dc81b90a994547
frankey darban <frankeydarban@gmail.com>
2019-08-01 00:41:46 -0700

    added some styling (added Google Font for titles, styled buttons, changed bg color and added titles with bg images)

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,27 @@
 $( document ).ready(function() {
 
-//connecting to Opentable API
-var queryURL = 'https://opentable.herokuapp.com/api';
+    //arrays and variables
+var restaurants = [
+    'placeholder 1', 'placeholder 2', 'placeholder 3'
+]
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-}).then(function(response){
-    console.log(response);
+//connecting to Opentable API
+    var queryURL = 'https://opentable.herokuapp.com/api';
+
+    $.ajax({
+        ///need to modify Line 13 with correct search query URL
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
 })
+
+
+
+
+
+
+
+
+
 });
