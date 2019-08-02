fbe0a13bed49326c0a8a6b2c43db99d025c1fd84
frankey darban <frankeydarban@gmail.com>
2019-08-01 21:45:02 -0700

    Conflict fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var baseOpenTableUrl = 'https://opentable.herokuapp.com/api';
 
-function doSmthWithRestaurants (restaurants) {
-    // do smth
+function listRestaurants (restaurants) {
+    console.log(restaurants);
 }
 
 function getRestaurantsByZip (zip) {
@@ -9,10 +9,33 @@ function getRestaurantsByZip (zip) {
         ///need to modify Line 13 with correct search query URL
         url: baseOpenTableUrl + "/restaurants?zip=" + zip,
         method: 'GET'
-    }).then(function (response) {
-        doSmthWithRestaurants(response.restaurants);
-        console.log(response.restaurants);
     })
+    .then(function (response) {
+        listRestaurants(response.restaurants);
+    })
+}
+
+function listEvents(events) {
+    console.log(events)
+}
+
+function getEventsByZip (zip) {
+    $.ajax({
+        type:"GET",
+        url:"https://app.ticketmaster.com/discovery/v2/events.json?dates.localDate&postalCode=" 
+        + zip + "&apikey=4BGuvHwzMdA9AzDaqHDjGGVAV5oWB8m6",
+        async:true,
+        dataType: "json",
+        success: function(json) {
+            // console.log(json);
+            listEvents(json._embedded.events)
+            // Parse the response.
+            // Do other things.
+        },
+        error: function(xhr, status, err) {
+            // This time, we do not end up here!
+        }
+    });
 }
 
 $(document).ready(function () {
@@ -20,7 +43,11 @@ $(document).ready(function () {
     $("#zip-code-button").on("click", function(event) {
         event.preventDefault();
         var zipCode = $('#zip-code-input').val().trim();
+
         getRestaurantsByZip(zipCode);
+
+        getEventsByZip(zipCode);
+
     });
 
 });
